bdb3077b627191aa3d8ad762faf8fa265fd61584
Aron <aronlee5263@gmail.com>
2023-06-10 00:18:41 +0900

    firebase confing 설정
    - 홈페이지에서 프로젝트 생성한 후
    - vscode 에서 v9에 맞게 confing 파일 내용 추가

---
+++ reading-list/src/firebase/config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getfirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAscFNXFN8KB2zPWAOjPR1_MC8Moi91H7M",
+  authDomain: "readinglistapp-e7d6b.firebaseapp.com",
+  projectId: "readinglistapp-e7d6b",
+  storageBucket: "readinglistapp-e7d6b.appspot.com",
+  messagingSenderId: "83686294878",
+  appId: "1:83686294878:web:4e48ea246d96d97ae6bbcf",
+};
+
+initializeApp(firebaseConfig);
+
+const db = getfirestore();
+
+export { db };
